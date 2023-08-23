58fc12511159ee424e5749a130984bd2d3a3de56
sanchitBarjibhe <barjibhe99@gmail.com>
2023-08-23 14:04:08 +0530

    all project setup

---
+++ Client/src/pages/Appmenubar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function appmenubar() {
+    return (
+        <div className='flex h-20 w-11 justify-center bg-slate-400'>
+            html
+        </div>
+    )
+}
+
---
+++ Client/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Appmenubar from './Appmenubar'
+
+export default function index() {
+    return (
+        <Appmenubar />
+    )
+}
